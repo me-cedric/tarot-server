36e31322ae54c28a1ca09ebaa84794bb39672ae9
Cédric Meyer <mecedricod@gmail.com>
2020-08-10 22:32:39 +0200

    socket works

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -5,19 +5,27 @@ import {
   WebSocketServer,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  MessageBody,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
  
 @WebSocketGateway()
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
- 
+  players: any[] = []
+
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
- 
-  @SubscribeMessage('msgToServer')
-  handleMessage(client: Socket, payload: string): void {
-    this.server.emit('msgToClient', payload);
+
+  @SubscribeMessage('dealCards')
+  handleDealCards(): void {
+    this.server.emit('dealCards');
+  }
+
+  @SubscribeMessage('cardPlayed')
+  handleCardPlayed(@MessageBody() data: { sprite: string, isPlayerA: boolean }): void {
+    this.logger.log('cardPlayed');
+    this.server.emit('cardPlayed', data);
   }
  
   afterInit(server: Server): void {
@@ -25,10 +33,15 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
   }
  
   handleDisconnect(client: Socket): void {
-    this.logger.log(`Client disconnected: ${client.id}`);
+    this.logger.log(`A user disconnected: ${client.id}`);
+    this.players = this.players.filter(player => player !== client.id);
   }
  
   handleConnection(client: Socket, ...args: any[]): void {
     this.logger.log(`Client connected: ${client.id}`);
+    this.players.push(client.id);
+    if (this.players.length === 1) {
+      this.server.emit('isPlayerA');
+    }
   }
 }
